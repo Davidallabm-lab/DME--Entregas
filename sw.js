7a167b47b6706da3f3eedeeb24ae4c9a97b0518c
Davidallabm-lab <davidallabm@hotmail.com>
2026-04-27 13:35:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'dme-v3'
+const CACHE = 'dme-v4'
 const ARQUIVOS = ['./index.html','./encarregado.html','./gestor.html','./style.css','./manifest.json','./icon-192.png','./icon-512.png']
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ARQUIVOS)).then(()=>self.skipWaiting())) })
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())) })
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('./index.html')))) })
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) })
